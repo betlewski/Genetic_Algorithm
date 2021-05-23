1070601c56acde459d3a10a34ab2b41d1289ceeb
betlewski <szymonbetlewski@wp.pl>
2021-05-23 20:24:16 +0200

    Added function descriptions

--- src/sample/ga/FunctionType.java
+++ src/sample/ga/FunctionType.java
@@ -5,29 +5,38 @@ package sample.ga;
  */
 public enum FunctionType {
 
-    ACKLEYS("Ackley's", 15),
-    BOOTHS("Booth's", 900),
-    THREE_HUMP_CAMEL("Three-hump camel", 2200);
+    ACKLEYS("Ackley's", FunctionType.ACKLEYS_DESC),
+    BOOTHS("Booth's", FunctionType.BOOTHS_DESC),
+    THREE_HUMP_CAMEL("Three-hump camel", FunctionType.THREE_HUMP_CAMEL_DESC);
 
     private final String name;
-    private final double maxYRange;
+    private final String description;
 
-    FunctionType(String name, double maxYRange) {
+    FunctionType(String name, String description) {
         this.name = name;
-        this.maxYRange = maxYRange;
+        this.description = description;
     }
 
     public String getName() {
         return name;
     }
 
-    public double getMaxYRange() {
-        return maxYRange;
+    public String getDescription() {
+        return description;
     }
 
     @Override
     public String toString() {
-        return this.name;
+        return getName();
     }
+
+    private static final String ACKLEYS_DESC = "Optimization for Ackley's function...\n" +
+            "Domain is [5, 5]. Minimum value is 0 at point (0,0).";
+
+    private static final String BOOTHS_DESC = "Optimization for Booth's function...\n" +
+            "Domain is [5, 5]. Minimum value is 0 at point (1,3).";
+
+    private static final String THREE_HUMP_CAMEL_DESC = "Optimization for the Three-Hump Camel function...\n" +
+            "Domain is [5, 5]. Minimum value is 0 at point (0,0).";
 
 }
